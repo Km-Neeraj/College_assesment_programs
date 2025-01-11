ce166df2321f16a0e9a8ff8a915b65be1d108e27
Neeraj Yadav <neerajydv1113@gmail.com>
2025-01-11 16:13:23 +0530

    Add files via upload

---
+++ strrev_string.c
@@ -0,0 +1,10 @@
+
+   #include<stdio.h>
+   #include<string.h>
+   int main()
+   {
+                 char s[]="for";
+                 strrev(s);
+                 printf("%s",s);
+                 return 0;        
+   }
